1b7dcb04a16d49409414c2168922f8f667f25ddb
Janne Harju <janne.h.harju@gmail.com>
2017-07-19 21:44:49 +0300

    trying to solve temp file problem.

--- Controllers/FileUploadController.cs
+++ Controllers/FileUploadController.cs
@@ -135,7 +135,11 @@ namespace PlayList.Controllers
                     fileTrack.Name = getTrackName(fullpath);//hanki bändi ja kappale mp3 tiedoston metasta
                     _multiSourcePlaylistRepository.PostTrack(fileTrack);
                     ++lastOrder;
-                    System.Threading.Thread.Sleep(200);
+                    while(!System.IO.File.Exists(fullpath))
+                    {
+                        System.Threading.Thread.Sleep(200);
+                    }
+
                     System.IO.Directory.EnumerateFiles(uploads).ToList().ForEach(x=>
                     {
                         System.IO.File.Delete(x);
