89018633f39c9d35218a1705438ee228069c594b
Janne Harju <janne.h.harju@gmail.com>
2017-09-09 23:56:45 +0300

    Guid fixed.

--- Common/KeyHelper.cs
+++ Common/KeyHelper.cs
@@ -7,7 +7,7 @@ namespace PlayList
     {
         public static byte[] GenerateKey()
         {
-            return Guid("1e283e11-fe6b-43b8-a042-b46cc3873f0d").ToByteArray();
+            return (new Guid("1e283e11-fe6b-43b8-a042-b46cc3873f0d")).ToByteArray();
         }
     }
 }
