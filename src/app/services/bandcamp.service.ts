799c62b4354e32c24bc86d60b64654c18f28eee3
Janne Harju <janne.harju@leanware.fi>
2019-07-07 19:57:46 +0300

    Revert "tryeing to solve not module error."
    This reverts commit 6d9d40b8bbbf53a46b0ef6ce3e5131c4e6b757ad.

--- src/app/services/bandcamp.service.ts
+++ src/app/services/bandcamp.service.ts
@@ -12,7 +12,7 @@ import * as utils from '../../../node_modules/bandcamp-scraper/lib/utils.js';*/
 /*let bandcamp = require('../../../node_modules/bandcamp-scraper/lib/index'),
     htmlParser  = require('../../../node_modules/bandcamp-scraper/lib/htmlParser.js'),
     utils       = require('../../../node_modules/bandcamp-scraper/lib/utils.js');*/
-import * as htmlParser from '../../../node_modules/bandcamp-scraper/lib/htmlParser.js';
+// import * as htmlParser from '../../../node_modules/bandcamp-scraper/lib/htmlParser.js';
 
 export interface BandcampOptions {
   page: number;
@@ -31,7 +31,7 @@ export class BandcampService {
       })
       .toPromise()
       .then((res) => {
-        return htmlParser.parseSearchResults(res.text()) as SearchResult[];
+        return {} as SearchResult[];
       })
       .catch(this.handlePromiseError);
   }
@@ -44,7 +44,7 @@ export class BandcampService {
       })
       .toPromise()
       .then((res) => {
-        return htmlParser.parseAlbumInfo(res.text(), decodedUrl) as AlbumInfo;
+        return {} as AlbumInfo;
       })
       .catch(this.handlePromiseError);
   }
@@ -58,7 +58,7 @@ export class BandcampService {
       })
       .toPromise()
       .then((res) => {
-        return htmlParser.parseAlbumUrls(res.text(), decodedUrl) as string[];
+        return {} as string[];
       })
       .catch(this.handlePromiseError);
   }
--- src/empty.js
+++
@@ -1,3 +0,0 @@
-import {} from '../node_modules/cheerio-req/node_modules/htmlparser2/lib/WritableStream.js';
-import {} from '../node_modules/cheerio-req/node_modules/htmlparser2/lib/Stream.js';
-//export var Writable = require("../node_modules/cheerio-req/node_modules/htmlparser2/lib/WritableStream.js").Writable;
